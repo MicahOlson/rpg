a0f8d171d5deb0aa942ce460f27ab26130201b51
Geof Rosenmund <geof.rosenmunds.email@gmail.com>
2021-02-03 13:46:52 -0800

    Test 1 with a good fail
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

---
+++ __tests__/character.test.js
@@ -0,0 +1,12 @@
+import Character from '../src/character.js';
+
+describe('Character', () => {
+
+  test('Should create a character obj with properties', () =>
+  {
+    const newCharacter = new Character("Bill","Mage");
+    expect(newCharacter.name).toEqual("Bill");
+    expect(newCharacter.class).toEqual("Mage");
+
+  });
+});
